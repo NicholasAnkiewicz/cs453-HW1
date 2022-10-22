db7b6209ccc8c8adc80886e6db9c80c9222cf8ba
Nick <nankiewicz@umass.edu>
2022-10-21 20:56:11 -0400

    UDP

--- UDP_Server-Reliable.py
+++ UDP_Server-Reliable.py
@@ -6,31 +6,9 @@ try:
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind(("127.0.0.1", 65444))
     while True:
-        data = s.recvfrom(1024)
-        c_address = data[1]
-        data = data[0]
+        data, c_address = s.recvfrom(1024)
         data = data.decode()
         print(data)
-        data = data.split(" ")
-        if len(data) != 3 or len(data[0]) != 1:
-            data = "-1"
-        elif ((["+", "-", "*", "/"].count(data[0]) == 0)):
-            data = 620
-        elif (not data[1].strip("-").isdigit()):
-            data = 630
-        elif (not data[2].strip("-").isdigit()):
-            data = 630
-        elif (data[2] == "0" and data[0] == "/"):
-            data = 630
-        else:
-            if (data[0] == "+"):
-                data = int(data[1]) + int(data[2])            
-            elif data[0] == "-":
-                data = int(data[1]) - int(data[2])
-            elif data[0] == "*":
-                data = int(data[1]) * int(data[2])
-            elif data[0] == "/":
-                data = int(data[1]) / int(data[2])
         s.sendto(str(data).encode(), c_address)
 
 except KeyboardInterrupt:
