ab1d35c1768517e615400bb810f431ddf0799d9a
Nick <nankiewicz@umass.edu>
2022-10-21 21:21:35 -0400

    sad

--- TCP_Client.py
+++ TCP_Client.py
@@ -12,7 +12,6 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("127.0.0.1", 65444))
 for line in input_lines:
     line = line.strip('\n')
-    print("Sending ", line, "to 127.0.0.1")
     s.sendall((line).encode())
     data = s.recv(1024)
     data = data.decode()
